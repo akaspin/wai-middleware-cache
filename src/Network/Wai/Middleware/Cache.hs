b372e99bbfb79f2d8518fa3f662d4cd8fc284058
Alexander Dorofeev <aka.spin@gmail.com>
2012-02-11 04:39:52 +0500

    release
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- src/Network/Wai/Middleware/Cache.hs
+++ src/Network/Wai/Middleware/Cache.hs
@@ -1,3 +1,4 @@
+
 -- | Transparent front cache middleware for 'Network.Wai'.
 --   
 --   Instead caching internal data, this middleware caches entire responses.
@@ -5,22 +6,44 @@
 --   the internal structure and concentration of caching in the immediate 
 --   vicinity of the request is more than redeem them.
 --
---  > cache (hedisBackend defaultConnectInfo)
---  >       rawPathInfo 
---  >       (const ["cdn", "templates"])
---  >       lookupEtag
---  >
---  >
---  >
+--  > cache (debugBackend True)
+--  >       ourFrivolousApplication
  
-module Network.Wai.Middleware.Cache where
+module Network.Wai.Middleware.Cache (
+    -- * Backend
+    CacheBackend,
+    -- * Middleware
+    cache
+) where
 
-import Data.ByteString (ByteString)
+import Data.Maybe (fromMaybe)
+import Data.ByteString.Lazy (empty)
+import Data.Conduit (ResourceT)
 
-import Network.Wai (Request(..))
+import Network.Wai (Application, Middleware, Request(..), Response(..),
+        responseLBS)
+import Network.HTTP.Types (status304)
 
-cache :: 
-       (Request -> ByteString)         -- ^ Cache key function
-    -> (Request -> [ByteString])       -- ^ Cache tags function 
-    -> (Request -> ByteString)         -- ^ 
-cache = undefined
+-- | Abstract cache backend. Result may be 'Nothing' you need to respond  
+--   wirh status @304 - Not Modified@.
+type CacheBackend =
+       Application      -- ^ Application
+    -> Request          -- ^ Request
+    -> ResourceT IO (Maybe Response)
+
+-- | Cache middleware. Use it with conjuction with 'CacheBackend'.  
+--  
+--  > -- Simplest backend. Suggests @304 - Not Modified@ with site root.
+--  > rootBackend app req = do 
+--  >     case rawPathInfo req of
+--  >         "/" -> return Nothing
+--  >         _ -> do
+--  >             res <- app req 
+--  >             return $ Just res
+cache ::
+       CacheBackend     -- ^ Cache backend.
+    -> Middleware
+cache cacheBackend app req = do
+    res <- cacheBackend app req
+    return $ fromMaybe (responseLBS status304 [] empty) res 
+
