9ed240cfcfc3358ad9bfbef72dfd0dd79f105ff6
Alexander Dorofeev <aka.spin@gmail.com>
2012-04-20 11:50:38 +0600

    `headerETag`, tests, version bump
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- src/Network/Wai/Middleware/Cache.hs
+++ src/Network/Wai/Middleware/Cache.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-} 
-{-# LANGUAGE OverloadedStrings #-} 
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-} 
 
 -- | Transparent front cache middleware for 'Network.Wai'.
 --   
@@ -16,21 +16,36 @@ module Network.Wai.Middleware.Cache (
     CacheBackend,
     CacheBackendError(..),
     cache,
+    headerETag,
     
     -- * Request helpers
-    lookupETag,
+    lookupETag
 ) where
+
+import Prelude hiding (concatMap)
 
 import Control.Exception (Exception)
 
+import Numeric (showHex)
+
+import Data.Word (Word8)
 import Data.Maybe (fromMaybe)
 import Data.Typeable (Typeable)
-import Data.ByteString (ByteString)
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
 import Data.ByteString.Lazy (empty)
-import Data.Conduit (ResourceT)
+
+import Data.Digest.Pure.MD5 (MD5Digest)
+import Data.Serialize (encode)
+
+import Data.Conduit (ResourceT, ($=), ($$), Flush(..))
+import qualified Data.Conduit.List as CL
+import Data.Conduit.Blaze (builderToByteStringFlush)
+import Crypto.Conduit (sinkHash)
 
 import Network.Wai (Application, Middleware, Request(..), Response(..),
-        responseLBS)
+        responseLBS, responseSource)
 import Network.HTTP.Types (status304)
 
 -- | Abstract cache backend. Result may be 'Nothing' you need to respond  
@@ -42,11 +57,12 @@ type CacheBackend =
 
 -- | Cache backend can throw errors. For handle this, use, for example,
 --   "Network.Wai.Middleware.Catch".
-data CacheBackendError = CacheBackendError ByteString
+data CacheBackendError = CacheBackendError B.ByteString
     deriving (Show, Eq, Typeable)
 instance Exception CacheBackendError
 
--- | Cache middleware. Use it with conjuction with 'CacheBackend'.  
+-- | Cache middleware. Use it with conjuction with 'CacheBackend' and 
+--   'headerETag'.  
 --  
 --  > -- Simplest backend. Suggests @304 - Not Modified@ with site root.
 --  > rootBackend app req = do 
@@ -55,19 +71,52 @@ instance Exception CacheBackendError
 --  >         _ -> do
 --  >             res <- app req 
 --  >             return $ Just res
+--  > app = responseLBS ok200 [] "someresponse"
+--  > 
+--  > cachedApp = cache rootBackend $ headerETag $ app
 cache ::
        CacheBackend     -- ^ Cache backend.
     -> Middleware
 cache cacheBackend app req = do
     res <- cacheBackend app req
     return $ fromMaybe (responseLBS status304 [] empty) res 
+
+-- | Add \"ETag\" header to response if it not present. Value of header is 
+--   @MD5@ hash of response body.
+headerETag :: Middleware
+headerETag app req = do
+    res <- app req
+    let (rs, rh, rsrc) = responseSource res
+    case lookup "etag" rh of
+        (Just _) -> return res
+        Nothing -> do
+            digest <- rsrc $= builderToByteStringFlush $= 
+                    CL.map fromChunk $$ sinkHash
+            let hash = toHex . encode $ (digest :: MD5Digest)
+            return $ ResponseSource rs (("ETag", hash):rh) rsrc
+  where
+    fromChunk (Chunk a) = a
+    fromChunk Flush = ""
+    
+    -- | Convert to hex
+    toHex :: B.ByteString -> B.ByteString
+    toHex =
+        B.concatMap word8ToHex
+      where
+        word8ToHex :: Word8 -> B.ByteString
+        word8ToHex w = B8.pack $ pad $ showHex w []
+        -- We know that the input will always be 1 or 2 characters long.
+        pad :: String -> String
+        pad [x] = ['0', x]
+        pad s   = s
     
 ----------------------------------------------------------------------------
 -- Request Helpers
 ----------------------------------------------------------------------------
 
--- | Helper for extract @If-None-Match@ header from 'Request'.
-lookupETag :: Request -> Maybe ByteString
+-- | Helper for extract @If-None-Match@ header from 'Request'. Use this with 
+--   backends.
+lookupETag :: Request -> Maybe B.ByteString
 lookupETag = lookup "If-None-Match" . requestHeaders
 
 
--- test/Main.hs
+++ test/Main.hs
@@ -4,26 +4,73 @@ module Main (main) where
 
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (Assertion, (@=?))
+import Test.HUnit (Assertion)
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 
 import Network.Wai (Application, responseLBS)
 import Network.Wai.Test
 import qualified Network.HTTP.Types as H
 
-import Control.Monad.IO.Class (liftIO)
+import Network.Wai.Middleware.Cache
 
 main :: IO ()
 main = defaultMain [
-        testCase "Just test" caseUncached
+        testCase "App without ETag" caseSimple,
+        testCase "App with Generated ETag" caseETag,
+        testCase "Generated ETag" caseEmbeddedEtag
     ]
+
+-- | Just test simple app
+caseSimple :: Assertion
+caseSimple = flip runSession appSimple $ do
+    r <- defRequest
+    assertResponseBase r
+    assertNoHeader "etag" r
+
+caseETag :: Assertion
+caseETag = flip runSession (headerETag appSimple) $ do
+    r <- defRequest
+    assertResponseBase r
+    assertHeader "etag" hashedData r
+
+caseEmbeddedEtag :: Assertion
+caseEmbeddedEtag = flip runSession appWithETag $ do
+    r <- defRequest
+    assertResponseBase r
+    assertHeader "etag" "no-match" r
+
+defRequest :: Session SResponse
+defRequest = request defaultRequest
+
+----------------------------------------------------------------------------
+-- Assertions
+----------------------------------------------------------------------------
+
+assertResponseBase :: SResponse -> Session ()
+assertResponseBase r = do
+    assertStatus 200 r
+    assertBody sourceData r
+
+----------------------------------------------------------------------------
+-- Applications
+----------------------------------------------------------------------------
     
-caseUncached :: Assertion
-caseUncached = flip runSession testApp $ do
-    r <- request defaultRequest
-    liftIO $ print r
-    
-testApp :: Application
-testApp _ = do
-    liftIO $ print "test" 
-    return $ responseLBS H.ok200 [] "short"
+appSimple :: Application
+appSimple _ = return $ responseLBS H.ok200 [] sourceData
+
+appWithETag :: Application
+appWithETag _ = return $ responseLBS H.ok200 
+        [("ETag", "no-match")] sourceData
+
+----------------------------------------------------------------------------
+-- Data
+----------------------------------------------------------------------------
+
+sourceData :: BL.ByteString
+sourceData = BL.fromChunks $ replicate 100000 "Hash response mock"
+
+hashedData :: B.ByteString
+hashedData = "385765267c8bd0154b5303b894dc34e7"
+
